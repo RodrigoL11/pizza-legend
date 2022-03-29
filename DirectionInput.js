611da0452cb9ab9ea0236ad64e4d21c70f53d563
RodrigoL11 <62080426+RodrigoL11@users.noreply.github.com>
2022-03-28 21:27:05 -0300

    Add files via upload

--- DirectionInput.js
+++ DirectionInput.js
@@ -1,38 +1,36 @@
 class DirectionInput {
-    constructor(config){
-        this.heldDirections = [];
-    
-        this.map = {
-            "ArrowUp": "up",
-            "KeyW": "up",
-            "ArrowDown": "down",
-            "KeyS": "down",
-            "ArrowLeft": "left",
-            "KeyA": "left",
-            "ArrowRight": "right",
-            "KeyD": "right",
-        }
-    }
+  constructor() {
+    this.heldDirections = [];
 
-    get direction() {
-        return this.heldDirections[0];
+    this.map = {
+      "ArrowUp": "up",
+      "KeyW": "up",
+      "ArrowDown": "down",
+      "KeyS": "down",
+      "ArrowLeft": "left",
+      "KeyA": "left",
+      "ArrowRight": "right",
+      "KeyD": "right",
     }
+  }
 
-    init() {
-        document.addEventListener("keydown", e => {
-            const dir = this.map[e.code];
-            if (dir && this.heldDirections.indexOf(dir) === -1) {
-                this.heldDirections.unshift(dir);
-                console.log(this.heldDirections);
-            }
-        });
-        document.addEventListener("keyup", e => {
-            const dir = this.map[e.code];
-            const index = this.heldDirections.indexOf(dir);
-            if (index > -1) {
-                this.heldDirections.splice(index, 1);
-                console.log(this.heldDirections);
-            }
-        });
-    }
+  get direction() {
+    return this.heldDirections[0];
+  }
+
+  init() {
+    document.addEventListener("keydown", e => {
+      const dir = this.map[e.code];
+      if (dir && this.heldDirections.indexOf(dir) === -1) {
+        this.heldDirections.unshift(dir);
+      }
+    });
+    document.addEventListener("keyup", e => {
+      const dir = this.map[e.code];
+      const index = this.heldDirections.indexOf(dir);
+      if (index > -1) {
+        this.heldDirections.splice(index, 1);
+      }
+    })
+  }
 }
--- GameObject.js
+++ GameObject.js
@@ -1,15 +1,23 @@
 class GameObject {
-    constructor(config) {
-        this.x = config.x || 0
-        this.y = config.y || 0
-        this.direction = config.direction || 'down'
-        this.sprite = new Sprite({
-            gameObject: this,
-            src: config.src || '/images/characters/people/hero.png',
-        });
-    }
+  constructor(config) {
+    this.id = null;
+    this.isMounted = false;
+    this.x = config.x || 0;
+    this.y = config.y || 0;
+    this.direction = config.direction || "down";
+    this.sprite = new Sprite({
+      gameObject: this,
+      src: config.src || "/images/characters/people/hero.png",
+    });
+  }
 
-    update() {
+  mount(map) {
+    console.log('mounting')
+    this.isMounted = true;
+    map.addWall(this.x, this.y);
+  }
 
-    }
+  update() {
+
+  }
 }
